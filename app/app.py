aea9eff252364fbdfd544194be1e908c1b3233f2
wasurus <wasurus.w@gmail.com>
2025-12-18 15:25:34 +0700

    Refactor DSSAuth class and integrate Flask-SocketIO for improved MQTT handling

--- app/app.py
+++ app/app.py
@@ -12,7 +12,9 @@ from binascii import unhexlify
 from Crypto.Util.Padding import unpad
 from Crypto.Cipher import AES
 import paho.mqtt.client as mqtt
+from flask_socketio import SocketIO
 from dotenv import load_dotenv
+from dss_auth import DSSAuth
 
 load_dotenv()
 
@@ -31,10 +33,12 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 app.config["MQTT_BROKER_URL"] = os.getenv("MQTT_BROKER_URL")
 app.config["MQTT_BROKER_PORT"] = int(os.getenv("MQTT_BROKER_PORT", 1883))
 app.config["MQTT_USERNAME"] = os.getenv("MQTT_USERNAME")
-app.config["MQTT_PASSWORD"] = os.getenv("MQTT_PASSWORD")
 app.config["MQTT_KEEPALIVE"] = int(os.getenv("MQTT_KEEPALIVE", 60))
 app.config["MQTT_TLS_ENABLED"] = os.getenv("MQTT_TLS_ENABLED") == "True"
 mqtt = Mqtt(app)
+socketio = SocketIO(app)
+
+dssAuth = DSSAuth()
 
 with app.app_context():
     # db.drop_all()
@@ -43,15 +47,12 @@ with app.app_context():
 
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
+    dss_mq_password, _ = dssAuth.get_dss_mq_password()
+    logging.INFO("DSS MQTT Password: %s", dss_mq_password)
     if rc == 0:
         app.logger.info("Connected to MQTT Broker!")
     else:
         app.logger.error("Failed to connect, return code %d\n", rc)
-
-
-@mqtt.on_disconnect()
-def handle_disconnect(client, userdata, rc):
-    app.logger.info("Disconnected from MQTT Broker with return code %d", rc)
 
 
 @mqtt.on_message()
@@ -61,10 +62,15 @@ def handle_mqtt_message(client, userdata, message):
     )
 
 
+@mqtt.on_log()
+def handle_logging(client, userdata, level, buf):
+    print(level, buf)
+
+
 @app.route("/")
 def index():
     return "ISOC Backend is running."
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3333, debug=os.getenv("DEBUG"))
+    socketio.run(app, host="0.0.0.0", port=3333, debug=os.getenv("DEBUG"), use_reloader=False)
--- app/dss_auth.py
+++ app/dss_auth.py
@@ -20,83 +20,84 @@ dss_api_url = os.getenv("DSS_API_URL")
 dss_username = os.getenv("DSS_USERNAME")
 dss_password = os.getenv("DSS_PASSWORD")
 
-
-def first_authentication():
-    response = requests.post(
-        url="https://" + dss_api_url + "/brms/api/v1.0/accounts/authorize",
-        json={"userName": dss_username, "clientType": "WINPC_V2"},
-        verify=False,
-    ).json()
-    return response
-
-
-def get_signature(realm, randomKey):
-    temp1 = hashlib.md5(dss_password.encode("utf-8")).hexdigest()
-    temp2 = hashlib.md5(f"{dss_username}{temp1}".encode("utf-8")).hexdigest()
-    temp3 = hashlib.md5(temp2.encode("utf-8")).hexdigest()
-    temp4 = hashlib.md5(f"{dss_username}:{realm}:{temp3}".encode("utf-8")).hexdigest()
-    signature = hashlib.md5(f"{temp4}:{randomKey}".encode("utf-8")).hexdigest()
-    return signature
+class DSSAuth:
+    def first_authentication(self):
+        response = requests.post(
+            url="https://" + dss_api_url + "/brms/api/v1.0/accounts/authorize",
+            json={"userName": dss_username, "clientType": "WINPC_V2"},
+            verify=False,
+        ).json()
+        return response
 
 
-def second_authentication(signature, randomKey, publicKey):
-    data = {
-        "userName": dss_username,
-        "signature": signature,
-        "randomKey": randomKey,
-        "publicKey": publicKey,
-        "encryptType": "MD5",
-        "ipAddress": "",
-        "clientType": "WINPC_V2",
-        "userType": "0",
-    }
-    response = requests.post(
-        url="https://" + dss_api_url + "/brms/api/v1.0/accounts/authorize",
-        json=data,
-        verify=False,
-    ).json()
-    return response
+    def get_signature(self, realm, randomKey):
+        temp1 = hashlib.md5(dss_password.encode("utf-8")).hexdigest()
+        temp2 = hashlib.md5(f"{dss_username}{temp1}".encode("utf-8")).hexdigest()
+        temp3 = hashlib.md5(temp2.encode("utf-8")).hexdigest()
+        temp4 = hashlib.md5(f"{dss_username}:{realm}:{temp3}".encode("utf-8")).hexdigest()
+        signature = hashlib.md5(f"{temp4}:{randomKey}".encode("utf-8")).hexdigest()
+        return signature
 
 
-def get_mq_credentials(token):
-    response = requests.post(
-        url="https://" + dss_api_url + "/brms/api/v1.0/BRM/Config/GetMqConfig",
-        headers={"X-Subject-Token": token},
-        json={},
-        verify=False,
-    ).json()
-    return response
+    def second_authentication(self, signature, randomKey, publicKey):
+        data = {
+            "userName": dss_username,
+            "signature": signature,
+            "randomKey": randomKey,
+            "publicKey": publicKey,
+            "encryptType": "MD5",
+            "ipAddress": "",
+            "clientType": "WINPC_V2",
+            "userType": "0",
+        }
+        response = requests.post(
+            url="https://" + dss_api_url + "/brms/api/v1.0/accounts/authorize",
+            json=data,
+            verify=False,
+        ).json()
+        return response
 
 
-def rsa_decrypt(word, private_key):
-    sentinel = Random.new().read(256)
-    ciphertext_bytes = base64.b64decode(word)
-    rsa_private_key = PKCS1_v1_5.new(RSA.import_key(private_key))
-    decrypted_text = rsa_private_key.decrypt(ciphertext_bytes, sentinel)
-    return decrypted_text
+    def get_mq_credentials(self, token):
+        response = requests.post(
+            url="https://" + dss_api_url + "/brms/api/v1.0/BRM/Config/GetMqConfig",
+            headers={"X-Subject-Token": token},
+            json={},
+            verify=False,
+        ).json()
+        return response
 
 
-def aes_decrypt(word, secret_key, secret_vector):
-    key = secret_key
-    iv = secret_vector
-    encrypted_hex_word = unhexlify(word)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    decrypted_word = unpad(cipher.decrypt(encrypted_hex_word), AES.block_size)
-    return decrypted_word.decode("utf-8")
+    def rsa_decrypt(self, word, private_key):
+        sentinel = Random.new().read(256)
+        ciphertext_bytes = base64.b64decode(word)
+        rsa_private_key = PKCS1_v1_5.new(RSA.import_key(private_key))
+        decrypted_text = rsa_private_key.decrypt(ciphertext_bytes, sentinel)
+        return decrypted_text
 
-def get_dss_mq_password():
-    key = RSA.generate(2048)
-    private_key = key.export_key().decode('utf-8')
-    public_key = key.publickey().export_key().decode('utf-8').replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "").replace("\n", "")
-    first_authentication_resp = first_authentication()
-    signature = get_signature(first_authentication_resp['realm'], first_authentication_resp['randomKey'])
-    second_authentication_resp = second_authentication(signature, first_authentication_resp['randomKey'], public_key)
-    secret_key = rsa_decrypt(second_authentication_resp['secretKey'], private_key)
-    secret_vector = rsa_decrypt(second_authentication_resp['secretVector'], private_key)
-    mq_credentials = get_mq_credentials(second_authentication_resp['token'])
-    decrypted_pass = aes_decrypt(mq_credentials['data']['password'], secret_key, secret_vector)
-    return decrypted_pass, mq_credentials['data']
 
-dss_mq_password, data = get_dss_mq_password()
-print(f"dss_mq_password: {dss_mq_password}")
-print(f"data: {data}")
+    def aes_decrypt(self, word, secret_key, secret_vector):
+        key = secret_key
+        iv = secret_vector
+        encrypted_hex_word = unhexlify(word)
+        cipher = AES.new(key, AES.MODE_CBC, iv)
+        decrypted_word = unpad(cipher.decrypt(encrypted_hex_word), AES.block_size)
+        return decrypted_word.decode("utf-8")
+
+    def get_dss_mq_password(self):
+        key = RSA.generate(2048)
+        private_key = key.export_key().decode('utf-8')
+        public_key = key.publickey().export_key().decode('utf-8').replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "").replace("\n", "")
+        first_authentication_resp = self.first_authentication()
+        signature = self.get_signature(first_authentication_resp['realm'], first_authentication_resp['randomKey'])
+        second_authentication_resp = self.second_authentication(signature, first_authentication_resp['randomKey'], public_key)
+        secret_key = self.rsa_decrypt(second_authentication_resp['secretKey'], private_key)
+        secret_vector = self.rsa_decrypt(second_authentication_resp['secretVector'], private_key)
+        mq_credentials = self.get_mq_credentials(second_authentication_resp['token'])
+        decrypted_pass = self.aes_decrypt(mq_credentials['data']['password'], secret_key, secret_vector)
+        print(f"dss_mq_password: {decrypted_pass}")
+        return decrypted_pass, mq_credentials['data']
+
+# dss_mq_password, data = get_dss_mq_password()
+# print(f"dss_mq_password: {dss_mq_password}")
+# print(f"data: {data}")
