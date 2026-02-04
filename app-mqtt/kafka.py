97478c5efb32d3c1b8505273a61cfe8888811880
wasurus <wasurus.w@gmail.com>
2026-02-04 10:55:02 +0700

    Update alarm test data in main function; modify alarmCode and alarmPicture, and restore extData structure

--- app-mqtt/kafka.py
+++ app-mqtt/kafka.py
@@ -140,43 +140,6 @@ def image_url_to_base64(image_url):
 
 
 def main():
-    # data_test = {
-    #     "method": "brms.notifyAlarms",
-    #     "info": [
-    #         {
-    #             "deviceCode": "1000012",
-    #             "channelSeq": 0,
-    #             "unitType": 1,
-    #             "unitSeq": 0,
-    #             "nodeType": "2",
-    #             "nodeCode": "1000012$1$0$0",
-    #             "alarmCode": "{55ceabaa65ca4e94a4505bc5340c634d}",
-    #             "alarmStat": "1",
-    #             "alarmType": "100002",
-    #             "alarmGrade": "1",
-    #             "alarmPicture": "https://192.168.29.200:443/v1/s/3232243144/9901/1a9b7adb-fce4-11f0-8923-b496913132ed/20260203/1/dsf_730d67f4-00af-11f1-bc88-b496913132ed_15595907_15958269.jpg",
-    #             "alarmDate": "1770109799",
-    #             "memo": "",
-    #             "extData": '{"enableDeviceContact":"0","faceRecognitionInfo":{"birthday":"","captureFaceImageUrl":"https://192.168.29.200:443/v1/s/3232243144/9901/1a9b7adb-fce4-11f0-8923-b496913132ed/20260203/1/dsf_730d67f4-00af-11f1-bc88-b496913132ed_15958269_15976098.jpg","gender":"0","nationality":"9999","personFaceImageUrl":"https://192.168.29.200:443/upload/obms/headPic/41487033@1@1770003398502.jpg","personId":"8Ujr4N83JuJ2cpjGWLti0fScLdLnn3","personName":"นัฐพล ท้วมทวยหาญ","personTypeId":"","personTypeName":"","repositoryId":"2","repositoryName":"Face Arming","similarity":"99","tel":""}}',
-    #             "linkVideoChannels": [],
-    #             "userIds": [],
-    #             "alarmSourceName": "IPC",
-    #             "gpsX": 0.0,
-    #             "gpsY": 0.0,
-    #             "ruleThreshold": 0,
-    #             "stayNumber": 0,
-    #             "planTemplateId": "",
-    #             "deviceName": "NVR-Face-5",
-    #             "linkedOutput": "0",
-    #             "linkedAudioLight": "0",
-    #             "linkedAudio": "0",
-    #             "mapIds": [""],
-    #             "linkedVoicePrompt": "0",
-    #             "voicePromptContent": "",
-    #             "allowViewLinkInfo": "1",
-    #         }
-    #     ],
-    # }
     data_test = {
         "method": "brms.notifyAlarms",
         "info": [
@@ -187,14 +150,14 @@ def main():
                 "unitSeq": 0,
                 "nodeType": "2",
                 "nodeCode": "1000012$1$0$0",
-                "alarmCode": "{06e859d060a243999a574b348ea26646}",
+                "alarmCode": "{55ceabaa65ca4e94a4505bc5340c634d}",
                 "alarmStat": "1",
                 "alarmType": "100002",
                 "alarmGrade": "1",
-                "alarmPicture": "https://192.168.29.200:443/v1/s/3232243144/9901/1a9b7adb-fce4-11f0-8923-b496913132ed/20260204/1/dsf_01a23902-016c-11f1-8170-b496913132ed_13002230_13327303.jpg",
-                "alarmDate": "1770171272",
+                "alarmPicture": "https://192.168.29.200:443/v1/s/3232243144/9901/1a9b7adb-fce4-11f0-8923-b496913132ed/20260203/1/dsf_730d67f4-00af-11f1-bc88-b496913132ed_15595907_15958269.jpg",
+                "alarmDate": "1770109799",
                 "memo": "",
-                "extData": '{"enableDeviceContact": "0","faceRecognitionInfo": {"birthday": "","captureFaceImageUrl": "https://192.168.29.200:443/v1/s/3232243144/9901/1a9b7adb-fce4-11f0-8923-b496913132ed/20260204/1/dsf_01a23902-016c-11f1-8170-b496913132ed_13327303_13365627.jpg","gender": "0","nationality": "9999","personFaceImageUrl": "https://192.168.29.200:443/upload/obms/headPic/jeueRlb5dvxMPRDDLR7qJbAIqmNrbz@1@1769746877236.jpg","personId": "jeueRlb5dvxMPRDDLR7qJbAIqmNrbz","personName": "อธิป ทรการ","personTypeId": "","personTypeName": "","repositoryId": "2","repositoryName": "Face Arming","similarity": "99","tel": "",},}',
+                "extData": '{"enableDeviceContact":"0","faceRecognitionInfo":{"birthday":"","captureFaceImageUrl":"https://192.168.29.200:443/v1/s/3232243144/9901/1a9b7adb-fce4-11f0-8923-b496913132ed/20260203/1/dsf_730d67f4-00af-11f1-bc88-b496913132ed_15958269_15976098.jpg","gender":"0","nationality":"9999","personFaceImageUrl":"https://192.168.29.200:443/upload/obms/headPic/41487033@1@1770003398502.jpg","personId":"8Ujr4N83JuJ2cpjGWLti0fScLdLnn3","personName":"นัฐพล ท้วมทวยหาญ","personTypeId":"","personTypeName":"","repositoryId":"2","repositoryName":"Face Arming","similarity":"99","tel":""}}',
                 "linkVideoChannels": [],
                 "userIds": [],
                 "alarmSourceName": "IPC",
@@ -214,6 +177,43 @@ def main():
             }
         ],
     }
+    # data_test = {
+    #     "method": "brms.notifyAlarms",
+    #     "info": [
+    #         {
+    #             "deviceCode": "1000012",
+    #             "channelSeq": 0,
+    #             "unitType": 1,
+    #             "unitSeq": 0,
+    #             "nodeType": "2",
+    #             "nodeCode": "1000012$1$0$0",
+    #             "alarmCode": "{06e859d060a243999a574b348ea26646}",
+    #             "alarmStat": "1",
+    #             "alarmType": "100002",
+    #             "alarmGrade": "1",
+    #             "alarmPicture": "https://192.168.29.200:443/v1/s/3232243144/9901/1a9b7adb-fce4-11f0-8923-b496913132ed/20260204/1/dsf_01a23902-016c-11f1-8170-b496913132ed_13002230_13327303.jpg",
+    #             "alarmDate": "1770171272",
+    #             "memo": "",
+    #             "extData": '{"enableDeviceContact": "0","faceRecognitionInfo": {"birthday": "","captureFaceImageUrl": "https://192.168.29.200:443/v1/s/3232243144/9901/1a9b7adb-fce4-11f0-8923-b496913132ed/20260204/1/dsf_01a23902-016c-11f1-8170-b496913132ed_13327303_13365627.jpg","gender": "0","nationality": "9999","personFaceImageUrl": "https://192.168.29.200:443/upload/obms/headPic/jeueRlb5dvxMPRDDLR7qJbAIqmNrbz@1@1769746877236.jpg","personId": "jeueRlb5dvxMPRDDLR7qJbAIqmNrbz","personName": "อธิป ทรการ","personTypeId": "","personTypeName": "","repositoryId": "2","repositoryName": "Face Arming","similarity": "99","tel": "",},}',
+    #             "linkVideoChannels": [],
+    #             "userIds": [],
+    #             "alarmSourceName": "IPC",
+    #             "gpsX": 0.0,
+    #             "gpsY": 0.0,
+    #             "ruleThreshold": 0,
+    #             "stayNumber": 0,
+    #             "planTemplateId": "",
+    #             "deviceName": "NVR-Face-5",
+    #             "linkedOutput": "0",
+    #             "linkedAudioLight": "0",
+    #             "linkedAudio": "0",
+    #             "mapIds": [""],
+    #             "linkedVoicePrompt": "0",
+    #             "voicePromptContent": "",
+    #             "allowViewLinkInfo": "1",
+    #         }
+    #     ],
+    # }
     resp = get_token()
     token = resp["token"]
     credential = resp["credential"]
